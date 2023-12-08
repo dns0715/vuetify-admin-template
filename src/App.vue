fc7d919ac2828e0b2531f1f7531cfbe51c4e4dc9
unseong <wox0715@naver.com>
2023-12-08 22:38:38 +0900

    editorconfig파일 생성

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    <router-link to="/">DashBoard</router-link> |
-    <router-link to="/grid-system">GridSystem</router-link>
-  </div>
-  <router-view/>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        permanent
+        theme="dark"
+      >
+        <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+        <v-list nav >
+          <v-list-item
+            link
+            v-for="item in items" 
+            :key="item.title"
+            :to="item.to"
+            :title="item.title"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px">
+        <router-view/>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
 export default {
+  setup(){
+    const items = [
+      {title: 'DashBoard', icon: 'mdi-view-dashboard', to: '/'},
+      {title: 'GridSystem', icon: 'mdi-view-dashboard', to: '/grid-system'},
+    ];
+    return {
+      items,
+    }
+  }
 }
 </script>
 <style>
