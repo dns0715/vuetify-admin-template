388bbf361471cc8f07e79f21e704c72ebaf9f6e6
unstar <wox0715@naver.com>
2023-12-12 14:18:40 +0900

    폴더구조변경

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
-import vuetify from './vuetify/index.js'
-
+import vuetify from './plugins/vuetify.js'
 createApp(App).use(router).use(vuetify).mount('#app')
